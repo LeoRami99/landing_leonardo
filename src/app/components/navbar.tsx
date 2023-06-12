d9363ad6b2aace810d448e12fb22e747756f4cbf
Leonardo Ramirez <juanlov4321@gmail.com>
2023-06-12 12:35:41 -0500

    Se añade para que se indexe en google

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,44 +1,49 @@
+const navbar = () => {
+	const styles = {
+		blur_navbar: {
+			backgroundColor: "rgba(0, 0, 0, 0.5)",
+			backdropFilter: "blur(10px)",
+			color: "#fff",
+		},
+	};
 
-const navbar = () =>{
-    const styles={
-      "blur_navbar":{
-        "backgroundColor": "rgba(0, 0, 0, 0.5)",
-        "backdropFilter": "blur(10px)",
-        "color": "#fff"
+	return (
+		<nav
+			className="navbar navbar-expand-lg fixed-top"
+			style={styles.blur_navbar}
+		>
+			<div className="container-fluid">
+				<a className="navbar-brand text-white" href="#">
+					Leonardo Ramirez
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarResponsive"
+					aria-controls="navbarResponsive"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarResponsive">
+					<ul className="navbar-nav ms-auto">
+						<li className="nav-item">
+							<a className="nav-link text-white" href="#yo">
+								Sobre mi
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-white" href="#skills">
+								Skills
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
-      }
-    }
-
-    return (
-        <nav className="navbar navbar-expand-lg  fixed-top" style={styles.blur_navbar}>
-          <div className="container-fluid">
-            <a className="navbar-brand text-white" href="#">
-              Leonardo Ramirez
-            </a>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item ">
-                  <a className="nav-link text-white" href="#yo">
-                    Sobre mi
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#skills">
-                    Skills
-                  </a>
-                </li>
-                </ul>
-            </div>
-
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
-              aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-        </nav>
-    )
-}
-
-export default navbar 
-
-
+export default navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 /* import './globals.css' */
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,6 +16,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* index follow */}
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Leonardo Ramirez" />
+        <meta name="keywords" content="Leonardo Ramirez, Leonardo, Ramirez, Portafolio, Portafolio Leonardo Ramirez, Leonardo Ramirez Portafolio" />
+        
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
